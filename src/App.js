65f7bcc552fdc1d678240371d533c816a7b3202e
Pranav Kalra <pkalra@sdsu.edu>
2022-01-30 16:49:17 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Navigation from './components/NavBar';
+import Home from './components/Home'
+import About from './components/About'
+import Tba from './components/Tba'
+import Garage from './components/Garage'
+import Manual from './components/Manual'
+import Crew from './components/Crew'
+import Track from './components/Track'
+import Faq from './components/Faq'
+import Footer from './components/Footer'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Home />
+      <About />
+      <Tba />
+      <Garage />
+      <Manual />
+      <Crew />
+      <Track />
+      <Faq />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
